d50e18ab88247e9bfff8e0b472948c63b33c5958
renatorcchaves <renato_chaves50@hotmail.com>
2024-12-24 10:03:16 -0300

    Atualizar arquivo home_v1.py

--- home_v1.py
+++ home_v1.py
@@ -29,62 +29,58 @@ condados = list(gdf_geo['name'].sort_values())
 # Dividindo a página do Streamlit em 2 colunas:
 coluna1, coluna2 = st.columns(2)
 
-with coluna1: 
-    # Adicionando os Input's widgets
-    selecionar_condado = st.selectbox("Condado", condados)
-    housing_median_age = st.number_input('Idade do imóvel', value=10, min_value=1, max_value=50)
-    median_income = st.slider('Renda média (milhares de US$)', min_value = 5.0, max_value = 100.0, value=45.0, step = 5.0)   # O max_value segue o valor máximo dessa variável no gdf_geo
+# Adicionando os Input's widgets
+selecionar_condado = st.selectbox("Condado", condados)
+housing_median_age = st.number_input('Idade do imóvel', value=10, min_value=1, max_value=50)
+median_income = st.slider('Renda média (milhares de US$)', min_value = 5.0, max_value = 100.0, value=45.0, step = 5.0)   # O max_value segue o valor máximo dessa variável no gdf_geo
 
-    # Devemos deixar widget's input pro usuário determinar todas as variáveis? São valores para um conjunto de imóveis. O usuário pode não saber que valor usar dependendo do caso
-    # Podemos deixar com um valor padrão por baixo dos panos que faça sentido pro modelo? 
-    # Usaremos o DADOS_GEO_MEDIAN para ver os valores MEDIANO das variáveis abaixo de acordo com o condado
-    longitude = gdf_geo.query("name == @selecionar_condado")['longitude'].values                 #llongitude = st.number_input('Longitude', value=-122.33)
-    latitude = gdf_geo[gdf_geo["name"] == selecionar_condado]["latitude"].values                 #latitude = st.number_input('Latitude', value=37.88)
-    total_rooms = gdf_geo.query("name == @selecionar_condado")['total_rooms'].values             #total_rooms = st.number_input('Total de cômodos', value=800)
-    total_bedrooms = gdf_geo.query("name == @selecionar_condado")['total_bedrooms'].values       #total_bedrooms = st.number_input('Total de quartos', value=100)
-    population = gdf_geo[gdf_geo["name"] == selecionar_condado]["population"].values             #population = st.number_input('População', value=300)
-    households = gdf_geo[gdf_geo["name"] == selecionar_condado]["households"].values             #households = st.number_input('Domicílios', value=100)
-    ocean_proximity = gdf_geo.query("name == @selecionar_condado")['ocean_proximity'].values     #ocean_proximity = st.selectbox('Proximidade do oceano', df['ocean_proximity'].unique())
-    rooms_per_household = gdf_geo.query("name == @selecionar_condado")['rooms_per_household'].values
-    bedrooms_per_room = gdf_geo.query("name == @selecionar_condado")['bedrooms_per_room'].values
-    population_per_household = gdf_geo.query("name == @selecionar_condado")['population_per_household'].values
+# Devemos deixar widget's input pro usuário determinar todas as variáveis? São valores para um conjunto de imóveis. O usuário pode não saber que valor usar dependendo do caso
+# Podemos deixar com um valor padrão por baixo dos panos que faça sentido pro modelo? 
+# Usaremos o DADOS_GEO_MEDIAN para ver os valores MEDIANO das variáveis abaixo de acordo com o condado
+longitude = gdf_geo.query("name == @selecionar_condado")['longitude'].values                 #llongitude = st.number_input('Longitude', value=-122.33)
+latitude = gdf_geo[gdf_geo["name"] == selecionar_condado]["latitude"].values                 #latitude = st.number_input('Latitude', value=37.88)
+total_rooms = gdf_geo.query("name == @selecionar_condado")['total_rooms'].values             #total_rooms = st.number_input('Total de cômodos', value=800)
+total_bedrooms = gdf_geo.query("name == @selecionar_condado")['total_bedrooms'].values       #total_bedrooms = st.number_input('Total de quartos', value=100)
+population = gdf_geo[gdf_geo["name"] == selecionar_condado]["population"].values             #population = st.number_input('População', value=300)
+households = gdf_geo[gdf_geo["name"] == selecionar_condado]["households"].values             #households = st.number_input('Domicílios', value=100)
+ocean_proximity = gdf_geo.query("name == @selecionar_condado")['ocean_proximity'].values     #ocean_proximity = st.selectbox('Proximidade do oceano', df['ocean_proximity'].unique())
+rooms_per_household = gdf_geo.query("name == @selecionar_condado")['rooms_per_household'].values
+bedrooms_per_room = gdf_geo.query("name == @selecionar_condado")['bedrooms_per_room'].values
+population_per_household = gdf_geo.query("name == @selecionar_condado")['population_per_household'].values
 
-    # Variáveis calculadas a partir das outras variáveis definidas acima (optei por usar a mediana dessas variáveis pelo gdf_geo)
-    bins_income_cat = [0, 1.5, 3, 4.5, 6, np.inf]
-    median_income_cat = np.digitize(median_income / 10, bins=bins_income_cat)                         #median_income_cat = st.number_input('Categoria de renda', value = 4)
-    # Essa função do numpu np.digitize recebe um número (median_income) e determina a qual categoria pertence esse número de acordo com os as divisões (bins) que ela receber
+# Variáveis calculadas a partir das outras variáveis definidas acima (optei por usar a mediana dessas variáveis pelo gdf_geo)
+bins_income_cat = [0, 1.5, 3, 4.5, 6, np.inf]
+median_income_cat = np.digitize(median_income / 10, bins=bins_income_cat)                         #median_income_cat = st.number_input('Categoria de renda', value = 4)
+# Essa função do numpu np.digitize recebe um número (median_income) e determina a qual categoria pertence esse número de acordo com os as divisões (bins) que ela receber
 
-    #rooms_per_household = (total_rooms / households)                                             #rooms_per_household = st.number_input('Cômodos por domícilio', value = 7)
-    #bedrooms_per_room = (total_bedrooms / total_rooms)                                           #bedrooms_per_room = st.number_input('Quartos por cômodo', value = 0.2)
-    #population_per_household = (population / households)                                         #population_per_household = st.number_input('Pessoas por domícilio', value = 2)
+#rooms_per_household = (total_rooms / households)                                             #rooms_per_household = st.number_input('Cômodos por domícilio', value = 7)
+#bedrooms_per_room = (total_bedrooms / total_rooms)                                           #bedrooms_per_room = st.number_input('Quartos por cômodo', value = 0.2)
+#population_per_household = (population / households)                                         #population_per_household = st.number_input('Pessoas por domícilio', value = 2)
 
-    botao_previsao = st.button("Prever preço")
+botao_previsao = st.button("Prever preço")
 
-    entrada_modelo = {
-    'longitude': longitude,
-    'latitude': latitude,
-    'housing_median_age': housing_median_age,
-    'total_rooms': total_rooms,
-    'total_bedrooms': total_bedrooms,
-    'population': population,
-    'households': households,
-    'median_income': median_income / 10,
-    'ocean_proximity': ocean_proximity,
-    'median_income_cat': median_income_cat,
-    'rooms_per_household': rooms_per_household,
-    'bedrooms_per_room': bedrooms_per_room,
-    'population_per_household': population_per_household,
-    }
+entrada_modelo = {
+'longitude': longitude,
+'latitude': latitude,
+'housing_median_age': housing_median_age,
+'total_rooms': total_rooms,
+'total_bedrooms': total_bedrooms,
+'population': population,
+'households': households,
+'median_income': median_income / 10,
+'ocean_proximity': ocean_proximity,
+'median_income_cat': median_income_cat,
+'rooms_per_household': rooms_per_household,
+'bedrooms_per_room': bedrooms_per_room,
+'population_per_household': population_per_household,
+}
 
-    df_entrada_modelo = pd.DataFrame(entrada_modelo, index=[0])        # index=[0] --> É só porque deu um erro no streamlit devido falta de índice 
+df_entrada_modelo = pd.DataFrame(entrada_modelo, index=[0])        # index=[0] --> É só porque deu um erro no streamlit devido falta de índice 
 
-    if botao_previsao:                                 # Se a pessoa tiver clicado no botão, essa variável será "Verdadeira"
-        try:
-            preco = modelo.predict(df_entrada_modelo)
-            st.metric(label="Preço previsto:", value=f"US$ {preco[0][0]:.2f}")
+if botao_previsao:                                 # Se a pessoa tiver clicado no botão, essa variável será "Verdadeira"
+    try:
+        preco = modelo.predict(df_entrada_modelo)
+        st.metric(label="Preço previsto:", value=f"US$ {preco[0][0]:.2f}")
 
-        except Exception as e:
-            st.error(f'Ocorreu um erro durante a previsao: {e}')
-
-with coluna2:
-    st.write('coluna2')
+    except Exception as e:
+        st.error(f'Ocorreu um erro durante a previsao: {e}')
